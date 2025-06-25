24a535daae3a7f4736db05764c7e3923aeddc434
otheng03 <07c00h@gmail.com>
2025-06-26 00:49:19 +0900

    Fixed DictEntry.kt

--- src/main/kotlin/dict/DictEntry.kt
+++ src/main/kotlin/dict/DictEntry.kt
@@ -1,4 +1,14 @@
 package org.example.dict
 
-class DictEntry {
+data class DictEntry(
+    val key: String,
+    var value: Value,
+    var next: DictEntry? = null
+) {
+    sealed class Value {
+        data class any(val data: Any?) : Value()
+        data class u64(val data: ULong) : Value()
+        data class s64(val data: Long) : Value()
+        data class d(val data: Double) : Value()
+    }
 }
