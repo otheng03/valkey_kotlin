6272144f0847e1894cbbdc88b073aa176fae90b1
otheng03 <07c00h@gmail.com>
2025-07-01 23:58:40 +0900

    Added some comments of Bucket

--- src/main/kotlin/hashtable/Bucket.kt
+++ src/main/kotlin/hashtable/Bucket.kt
@@ -1,4 +1,40 @@
 package valkey.kotlin.hashtable
 
-class Bucket {
-}
+const val ENTRIES_PER_BUCKET = 7
+typealias BUCKET_BITS_TYPE  = UByte
+const val BITS_NEEDED_TO_STORE_POS_WITHIN_BUCKET = 3
+
+/*
+ * Original Bucket of valkey is optimized to fit within a CPU cache line.
+ * In this kotlin toy project, the Bucket implementation is simplified for better understanding and easier understanding.
+ * In a 64-bit system, there are up to 7 entries per bucket.
+ * These are unordered and an entry can b inserted in any of the free slots.
+ *
+ *  * Bucket layout
+ *         +------------------------------------------------------------------+
+ *         | Metadata | Entry | Entry | Entry | Entry | Entry | Entry | Entry |
+ *         +------------------------------------------------------------------+
+ *        /            ` - - . _ _
+ *       /                         `- - . _ _
+ *      /                                     ` - . _
+ *     +----------------------------------------------+
+ *     | c ppppppp hash hash hash hash hash hash hash |
+ *     +----------------------------------------------+
+ *      |    |       |
+ *      |    |      One byte of hash for each entry position in the bucket.
+ *      |    |
+ *      |   Presence bits. One bit for each entry position, indicating if an
+ *      |   entry present or not.
+ *      |
+ *     Chained? One bit. If set, the last entry is a child bucket pointer.
+ *
+ * 64-bit version, 7 entries per bucket:
+ *     1 bit     7 bits    [1 byte] x 7  [8 bytes] x 7 = 64 bytes
+ *     chained   presence  hashes        entries
+ */
+data class HashtableBucket (
+    val chained: UByte,
+    val presence: UByte,
+    val hashes: ByteArray = ByteArray(ENTRIES_PER_BUCKET),
+    val entries: Array<Any?> = arrayOfNulls(ENTRIES_PER_BUCKET)
+)
